80c1bf971dfb1db57cdd43c1da22f57a04ee538b
haerim95 <tjffldi123@daum.net>
2021-03-06 00:11:46 +0900

    axios movie api 불러오기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component{
   state = {
     isLoading: true,
     movies: [],
   };
+  getMovie = async () =>{
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    console.log(movies);
+  }
   componentDidMount(){ 
-    //영화 데이터 로딩~!!!
-    setTimeout(()=>{
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovie();
   };
   render(){
     const { isLoading } = this.state;
