a9399e613868b8449d7e26295dff907c427cd688
haerim95 <tjffldi123@daum.net>
2021-03-11 00:03:48 +0900

    loading 후 메세지 불러오기

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ class App extends React.Component{
         data: { movies },
       },
     } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
-    console.log(movies);
+    // this.setState({ movies : movies }) // state : 구조분해 할당으로 얻은 영화 데이터 변수
+    // state와 axios.get()의 결과 값 변수의 이름이 같다고 둘을 혼동하면 안된다.
+
+    this.setState({ movies, isLoading : false }); // ES6에서는 객체의 키와 대입할 변수의 이름이 같다면 다음과 같이 'movies' 로 축약할 수 있다.
   }
   componentDidMount(){ 
     this.getMovie();
