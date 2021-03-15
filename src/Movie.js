cf4cddcbe391125382d1dfebc214b124c7011ad4
haerim95 <tjffldi123@daum.net>
2021-03-15 22:44:09 +0900

    뮤비 데이터 불러와서 화면에 뿌려주기

---
+++ src/Movie.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Movie.css';
+
+function Movie({title, year, summary, poster, genres}){
+    return (
+        <div className="movie">
+            <img src={poster}  alt={title} title={title}/>
+            <div className="movie_data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                 {/* 장르는 배열이므로 map() 함수를 사용하여 출력한다 */}
+                 <ul className="movie_genres">
+                    {genres.map((genre, index)=>{ //genre에는 genres의 배열 원소가, indexdpsms 1,2,3...번째임을 알리는 숫자가
+                        return <li key={index} className="movie__genre">{genre}</li>;
+                    })}
+                </ul>
+                <p className="movie__summary">{summary}</p>
+            </div>
+        </div>
+    );
+}
+
+Movie.propTypes = { //propTypes : props의 자료형을 검사하는 도구, 설치해서 사용, 전달받은 props가 맞는 값인지 확인해준다.
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired, // 이미지 주소가 문자열이므로 string
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired, //문자열 배일이 맞는지
+};
+
+export default Movie;
